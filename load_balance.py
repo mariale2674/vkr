7bd6fcc1b032038e236b02d9658bacf1007467f1
mariale2674 <39548576+mariale2674@users.noreply.github.com>
2018-06-02 23:34:22 +0300

    Добавила обработку запросов и очередь.

--- load_balance.py
+++ load_balance.py
@@ -3,6 +3,7 @@
 
 import random
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 
 N = 5
 M = 5
@@ -10,8 +11,9 @@ M = 5
 
 class LoadBalance:
     def __init__(self, u, u_max, X, time):
+        # self.N = N
         self.u = u
-        self.u_max = u.max
+        self.u_max = u_max
         self.X = X
         self.time = time
 
@@ -22,34 +24,85 @@ class LoadBalance:
         return k / d
 
     def flaw(self, g, n, present_lam, past_lam):
-        return (g / n) * (1 + (present_lam - past_lam) / past_lam)
+        b = (g / n) * (1 + (present_lam - past_lam) / past_lam)
+
+        if b == 0:
+            b = 0.1
+        if b > 1:
+            b = 1
+
+        return b
 
     def dist(self, d, b, past_b):
         return past_b * d / b
 
     def distribution(self, u, u_max, X, time):
-        k = 1  # k - шаг
+        k = 1
         d = 300
         n = 3
+        q = 0
+        q_max = 1000
+        q_types = []
+        lost = 0
         past_request = 0
-        Arr = np.array([0] * n)
-        arr = 0
         past_b = 0
-        # X = np.zeros((N, M))
+        past_u = np.array([0] * N)
 
         while k < 1000:
-            print("d", d)
             g = 0
+            arr = 0
+            Arr = np.array([0] * n)
             requests = random.randint(100, 1000)
-            array_of_types = np.array([0] * M)
 
+            # Распределение запросов (с учетом типа)
             for i in range(requests):
-                request_type = random.randint(0, 4)
-                array_of_types[request_type] += 1
+                array_of_types = random.randint(0, 4)
+                buf = [row[array_of_types] for row in X]
+                min_N = np.argmax(buf)
+                min_u = u[min_N]
 
-            max_n = np.argmax(array_of_types)
-            # max = np.max(array_of_types)
+                for element in buf:
+                    if (buf[element] == 1) and (u[element] < min_u):
+                        min_u = u[element]
+                        min_N = element
 
+                if u_max[min_N] < u[min_N] + 1:
+                    if q_max > q:
+                        q += 1
+                        q_types.append(array_of_types)
+                    else:
+                        lost += 1
+                else:
+                    u[min_N] += 1
+
+            # Обработка запросов
+            for i in range(N):
+                u[i] -= random.randint(0, time[i])
+                if u[i] < 0:
+                    u[i] = 0
+
+            # Обработка очереди
+            if q > 1:
+                for request in q_types:
+                    type = q_types[request]
+                    buf = [row[type] for row in X]
+                    min_N = np.argmax(buf)
+                    min_u = u[min_N]
+
+                    for element in buf:
+                        if (buf[element] == 1) and (u[element] < min_u):
+                            min_u = u[element]
+                            min_N = element
+
+                    if u_max[min_N] > u[min_N] + 1:
+                        u[min_N] += 1
+                        q -= 1
+                        q_types.remove(type)
+
+            # Подсчет прогнозируемой величины
+            forecast = u + past_u
+
+            # Сбор статистики
             present_lam = LoadBalance.lam(self, requests, d)
             if k == 1:
                 past_lam = LoadBalance.lam(self, random.randint(100, 1000), d)
@@ -69,47 +122,14 @@ class LoadBalance:
                     g += 1
 
             b = LoadBalance.flaw(self, g, n, present_lam, past_lam)
-            # Ограничение пачечности (0, 1]
-            if b == 0:
-                b = 0.1
-            if b > 1:
-                b = 1
-
-            print('b', b, '\npast_b', past_b, '\n')
-
             d = LoadBalance.dist(self, d, b, past_b)
-
-            if b > past_b:
-                # Уменьшаем d(k)
-                d -= 1
-            else:
-                if b < past_b:
-                    # Увеличиваем d(k)
-                    d += 1
-                else:
-                    d = d
 
             past_b = b
             past_request = requests
-
-            # Распределение по типам
-            # for i in range(N):
-            #     for j in range(M):
-            #         if max_n == j:
-            #             if X[i][j] == 1:
-            #                 u[i] += d
-            #                 print('Загруженность серверов', u)
-
-            u[np.argmin(u)] += requests
-
-            # Обработка запросов
-            for i in range(N):
-                u[i] -= random.randint(0, time[i])
-                if u[i] < 0:
-                    u[i] = 0
-
-            # Сделать очередь и переменную для потерянных запросов
-
-            print('Загруженность серверов', u)
-
+            past_u = u
             k += 1
+
+        print('Загруженность серверов', u)
+        print('Прогнозируемая загруженность', forecast)
+        print('Очередь', q)
+        print('Потерянные запросы', lost)
