f0bb4dfbdcd29a76cc6ffa26b2b1b8aa88137fac
mariale2674 <39548576+mariale2674@users.noreply.github.com>
2018-06-12 15:59:19 +0300

    Update window.py

--- window.py
+++ window.py
@@ -215,13 +215,13 @@ class ThreeServerWindow(tk.Frame):
     def build_graph(self, fig, ax1, ax2, ax3, canvas, u_max):
         try:
             file = open("txt/N1.txt", "r").read()
-            ThreeServerWindow.draw_graph(self, ax1, file)
+            ThreeServerWindow.draw_graph(self, ax1, u_max[0], file)
             ax1.set_ylabel('N1', fontsize=12)
             file = open("txt/N2.txt", "r").read()
-            ThreeServerWindow.draw_graph(self, ax2, file)
+            ThreeServerWindow.draw_graph(self, ax2, u_max[1], file)
             ax2.set_ylabel('N2', fontsize=12)
             file = open("txt/N3.txt", "r").read()
-            ThreeServerWindow.draw_graph(self, ax3, file)
+            ThreeServerWindow.draw_graph(self, ax3, u_max[2], file)
             ax3.set_ylabel('N3', fontsize=12)
 
             canvas.draw()
@@ -230,7 +230,7 @@ class ThreeServerWindow(tk.Frame):
         except IOError:
             print("An IOError has occurred!")
 
-    def draw_graph(self, ax, file):
+    def draw_graph(self, ax, u_max, file):
         xList = []
         yList = []
 
@@ -242,76 +242,268 @@ class ThreeServerWindow(tk.Frame):
                 xList.append(int(x))
                 yList.append(int(y))
 
-        ax.plot(xList, yList)
+        ax.plot(xList, yList, "#00A3E0", u_max, "r-", label="max_CPU")
 
 
 class FourServerWindow(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.label = tk.Label(self, text='Загруженность сети', font=LARGE_FONT)
-        self.label.grid(row=0, column=0, columnspan=3)
-
-        self.img = ImageTk.PhotoImage(Image.open("img/N3.jpg"))
+        self.img = ImageTk.PhotoImage(Image.open("img/N4.JPG"))
         self.topology = tk.Label(self, image=self.img)
-        self.topology.grid(row=1, column=0, columnspan=2)
+        self.topology.grid(row=0, column=0, sticky=tk.N, padx=7)
 
-        self.info = tk.Label(self, text='Информация:\n', width=50, height=5)
-        self.info.grid(row=2, column=0, columnspan=2)
+        self.label = tk.Label(self, text='Количество серверов: 4',
+                              font=LARGE_FONT)
+        self.label.grid(row=1, column=0)
 
-        self.button1 = tk.Button(self, text='Старт',
+        self.info = tk.Frame(self)
+        self.info.grid(row=2, column=0)
+        self.info_text1 = tk.Label(self.info, text='Загруженность:',
+                                  font=NORMAL_FONT)
+        self.info_text2 = tk.Label(self.info, text='Прогноз:',
+                                  font=NORMAL_FONT)
+        self.info_text3 = tk.Label(self.info, text='Очередь:',
+                                  font=NORMAL_FONT)
+        self.info_text4 = tk.Label(self.info, text='Потерянные запросы:',
+                                  font=NORMAL_FONT)
+        self.info_text1.grid(row=0, sticky=tk.W, padx=7)
+        self.info_text2.grid(row=1, sticky=tk.W, padx=7)
+        self.info_text3.grid(row=2, sticky=tk.W, padx=7)
+        self.info_text4.grid(row=3, sticky=tk.W, padx=7)
+
+        self.data_u = tk.Label(self.info, text=var_u,
+                               font=SMALL_FONT)
+        self.data_forecast = tk.Label(self.info, text=var_forecast,
+                               font=SMALL_FONT)
+        self.data_queue = tk.Label(self.info, text=var_queue,
+                               font=SMALL_FONT)
+        self.data_lost = tk.Label(self.info, text=var_lost,
+                               font=SMALL_FONT)
+        self.data_u.grid(row=0, column=2, sticky=tk.W, padx=7)
+        self.data_forecast.grid(row=1, column=2, sticky=tk.W, padx=7)
+        self.data_queue.grid(row=2, column=2, sticky=tk.W, padx=7)
+        self.data_lost.grid(row=3, column=2, sticky=tk.W, padx=7)
+
+        self.buttons = tk.Frame(self)
+        self.buttons.grid(row=3, column=0)
+        self.button1 = tk.Button(self.buttons, text='Старт',
                                  command=lambda: FourServerWindow.start(self))
-        self.button1.grid(row=3, column=0, pady=7)
-        self.button2 = tk.Button(self, text='Назад',
+        self.button1.grid(row=0, column=0, pady=7)
+        self.button2 = tk.Button(self.buttons, text='Назад',
                                  command=lambda: controller.show_frame(StartPage))
-        self.button2.grid(row=3, column=1, pady=7)
+        self.button2.grid(row=0, column=1, pady=7)
+
+        self.fig = Figure()
+        self.ax1 = self.fig.add_subplot(411)
+        self.ax2 = self.fig.add_subplot(412)
+        self.ax3 = self.fig.add_subplot(413)
+        self.ax4 = self.fig.add_subplot(414)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=1, rowspan=5, sticky=tk.N+tk.E)
 
     def start(self):
         self.N = 4
         self.u = np.array([0] * self.N)
-        self.u_max = np.array([3000, 4000, 5000, 4000])
-        self.time = np.array([100, 150, 200, 300])
+        self.u_max = np.array([4000, 4000, 4000, 4000])
+        self.time = np.array([200, 200, 200, 200])
         self.X = np.array([[1, 0, 0, 0, 1],
-                           [0, 1, 0, 0, 0],
-                           [0, 0, 1, 0, 0],
-                           [0, 0, 0, 1, 0]])
+                           [0, 1, 0, 0, 1],
+                           [0, 0, 1, 0, 1],
+                           [0, 0, 0, 1, 1]])
 
         LB = lb.LoadBalance(self.u, self.u_max, self.X, self.time, self.N)
         LB.distribution(LB.u, LB.u_max, LB.X, LB.time, LB.N)
+
+        FourServerWindow.build_graph(self, self.fig, self.ax1, self.ax2, self.ax3, self.ax4, self.canvas, self.u_max)
+
+        try:
+            pullData = open("txt/buf.txt", "r").read()
+            dataList = pullData.split('\n')
+
+            self.data_u = tk.Label(self.info, text=dataList[0],
+                                   font=SMALL_FONT)
+            self.data_forecast = tk.Label(self.info, text=dataList[1],
+                                          font=SMALL_FONT)
+            self.data_queue = tk.Label(self.info, text=dataList[2],
+                                       font=SMALL_FONT)
+            self.data_lost = tk.Label(self.info, text=dataList[3],
+                                      font=SMALL_FONT)
+            self.data_u.grid(row=0, column=2, sticky=tk.W, padx=7)
+            self.data_forecast.grid(row=1, column=2, sticky=tk.W, padx=7)
+            self.data_queue.grid(row=2, column=2, sticky=tk.W, padx=7)
+            self.data_lost.grid(row=3, column=2, sticky=tk.W, padx=7)
+        except IOError:
+            print("An IOError has occurred!")
+
+    def build_graph(self, fig, ax1, ax2, ax3, ax4, canvas, u_max):
+        try:
+            file = open("txt/N1.txt", "r").read()
+            FourServerWindow.draw_graph(self, ax1, u_max[0], file)
+            ax1.set_ylabel('N1', fontsize=12)
+            file = open("txt/N2.txt", "r").read()
+            FourServerWindow.draw_graph(self, ax2, u_max[1], file)
+            ax2.set_ylabel('N2', fontsize=12)
+            file = open("txt/N3.txt", "r").read()
+            FourServerWindow.draw_graph(self, ax3, u_max[2], file)
+            ax3.set_ylabel('N3', fontsize=12)
+            file = open("txt/N4.txt", "r").read()
+            FourServerWindow.draw_graph(self, ax4, u_max[3], file)
+            ax4.set_ylabel('N4', fontsize=12)
+
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=0, column=1, rowspan=5, sticky=tk.N+tk.E)
+
+        except IOError:
+            print("An IOError has occurred!")
+
+    def draw_graph(self, ax, u_max, file):
+        xList = []
+        yList = []
+
+        ax_list = file.split('\n')
+
+        for eachLine in ax_list:
+            if len(eachLine) > 1:
+                x, y = eachLine.split(',')
+                xList.append(int(x))
+                yList.append(int(y))
+
+        ax.plot(xList, yList, "#00A3E0", u_max, "r-", label="max_CPU")
 
 
 class FiveServerWindow(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.label = tk.Label(self, text='Загруженность сети', font=LARGE_FONT)
-        self.label.grid(row=0, column=0, columnspan=3)
-
-        self.img = ImageTk.PhotoImage(Image.open("img/N3.jpg"))
+        self.img = ImageTk.PhotoImage(Image.open("img/N5.JPG"))
         self.topology = tk.Label(self, image=self.img)
-        self.topology.grid(row=1, column=0, columnspan=2)
+        self.topology.grid(row=0, column=0, sticky=tk.N, padx=7)
 
-        self.info = tk.Label(self, text='Информация:\n', width=50, height=5)
-        self.info.grid(row=2, column=0, columnspan=2)
+        self.label = tk.Label(self, text='Количество серверов: 5',
+                              font=LARGE_FONT)
+        self.label.grid(row=1, column=0)
 
-        self.button1 = tk.Button(self, text='Старт',
+        self.info = tk.Frame(self)
+        self.info.grid(row=2, column=0)
+        self.info_text1 = tk.Label(self.info, text='Загруженность:',
+                                  font=NORMAL_FONT)
+        self.info_text2 = tk.Label(self.info, text='Прогноз:',
+                                  font=NORMAL_FONT)
+        self.info_text3 = tk.Label(self.info, text='Очередь:',
+                                  font=NORMAL_FONT)
+        self.info_text4 = tk.Label(self.info, text='Потерянные запросы:',
+                                  font=NORMAL_FONT)
+        self.info_text1.grid(row=0, sticky=tk.W, padx=7)
+        self.info_text2.grid(row=1, sticky=tk.W, padx=7)
+        self.info_text3.grid(row=2, sticky=tk.W, padx=7)
+        self.info_text4.grid(row=3, sticky=tk.W, padx=7)
+
+        self.data_u = tk.Label(self.info, text=var_u,
+                               font=SMALL_FONT)
+        self.data_forecast = tk.Label(self.info, text=var_forecast,
+                               font=SMALL_FONT)
+        self.data_queue = tk.Label(self.info, text=var_queue,
+                               font=SMALL_FONT)
+        self.data_lost = tk.Label(self.info, text=var_lost,
+                               font=SMALL_FONT)
+        self.data_u.grid(row=0, column=2, sticky=tk.W, padx=7)
+        self.data_forecast.grid(row=1, column=2, sticky=tk.W, padx=7)
+        self.data_queue.grid(row=2, column=2, sticky=tk.W, padx=7)
+        self.data_lost.grid(row=3, column=2, sticky=tk.W, padx=7)
+
+        self.buttons = tk.Frame(self)
+        self.buttons.grid(row=3, column=0)
+        self.button1 = tk.Button(self.buttons, text='Старт',
                                  command=lambda: FiveServerWindow.start(self))
-        self.button1.grid(row=4, column=0, pady=7)
-        self.button2 = tk.Button(self, text='Назад',
+        self.button1.grid(row=0, column=0, pady=7)
+        self.button2 = tk.Button(self.buttons, text='Назад',
                                  command=lambda: controller.show_frame(StartPage))
-        self.button2.grid(row=4, column=1, pady=7)
+        self.button2.grid(row=0, column=1, pady=7)
+
+        self.fig = Figure()
+        self.ax1 = self.fig.add_subplot(511)
+        self.ax2 = self.fig.add_subplot(512)
+        self.ax3 = self.fig.add_subplot(513)
+        self.ax4 = self.fig.add_subplot(514)
+        self.ax5 = self.fig.add_subplot(515)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=1, rowspan=5, sticky=tk.N+tk.E)
 
     def start(self):
         self.N = 5
         self.u = np.array([0] * self.N)
-        self.u_max = np.array([1000, 2000, 3000, 4000, 5000])
-        self.time = np.array([300, 200, 200, 300, 400])
+        self.u_max = np.array([4000, 4000, 4000, 4000, 4000])
+        self.time = np.array([200, 200, 200, 200, 200])
         self.X = np.array([[1, 0, 0, 0, 0],
                            [0, 1, 0, 0, 0],
                            [0, 0, 1, 0, 0],
-                           [0, 1, 1, 1, 0],
-                           [1, 1, 0, 1, 1]])
+                           [0, 0, 0, 1, 0],
+                           [0, 0, 0, 0, 1]])
 
         LB = lb.LoadBalance(self.u, self.u_max, self.X, self.time, self.N)
         LB.distribution(LB.u, LB.u_max, LB.X, LB.time, LB.N)
+
+        FiveServerWindow.build_graph(self, self.fig, self.ax1, self.ax2, self.ax3, self.ax4, self.ax5, self.canvas, self.u_max)
+
+        try:
+            pullData = open("txt/buf.txt", "r").read()
+            dataList = pullData.split('\n')
+
+            self.data_u = tk.Label(self.info, text=dataList[0],
+                                   font=SMALL_FONT)
+            self.data_forecast = tk.Label(self.info, text=dataList[1],
+                                          font=SMALL_FONT)
+            self.data_queue = tk.Label(self.info, text=dataList[2],
+                                       font=SMALL_FONT)
+            self.data_lost = tk.Label(self.info, text=dataList[3],
+                                      font=SMALL_FONT)
+            self.data_u.grid(row=0, column=2, sticky=tk.W, padx=7)
+            self.data_forecast.grid(row=1, column=2, sticky=tk.W, padx=7)
+            self.data_queue.grid(row=2, column=2, sticky=tk.W, padx=7)
+            self.data_lost.grid(row=3, column=2, sticky=tk.W, padx=7)
+        except IOError:
+            print("An IOError has occurred!")
+
+    def build_graph(self, fig, ax1, ax2, ax3, ax4, ax5, canvas, u_max):
+        try:
+            file = open("txt/N1.txt", "r").read()
+            FiveServerWindow.draw_graph(self, ax1, u_max[0], file)
+            ax1.set_ylabel('N1', fontsize=12)
+            file = open("txt/N2.txt", "r").read()
+            FiveServerWindow.draw_graph(self, ax2, u_max[1], file)
+            ax2.set_ylabel('N2', fontsize=12)
+            file = open("txt/N3.txt", "r").read()
+            FiveServerWindow.draw_graph(self, ax3, u_max[2], file)
+            ax3.set_ylabel('N3', fontsize=12)
+            file = open("txt/N4.txt", "r").read()
+            FiveServerWindow.draw_graph(self, ax4, u_max[3], file)
+            ax4.set_ylabel('N4', fontsize=12)
+            file = open("txt/N5.txt", "r").read()
+            FiveServerWindow.draw_graph(self, ax5, u_max[4], file)
+            ax5.set_ylabel('N5', fontsize=12)
+
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=0, column=1, rowspan=5, sticky=tk.N+tk.E)
+
+        except IOError:
+            print("An IOError has occurred!")
+
+    def draw_graph(self, ax, u_max, file):
+        xList = []
+        yList = []
+
+        ax_list = file.split('\n')
+
+        for eachLine in ax_list:
+            if len(eachLine) > 1:
+                x, y = eachLine.split(',')
+                xList.append(int(x))
+                yList.append(int(y))
+
+        ax.plot(xList, yList, "#00A3E0", u_max, "r-", label="max_CPU")
 
 
 def check_files():
@@ -335,7 +527,6 @@ def main():
     check_files()
 
     app = LoadBalance()
-    # app.geometry("1000x500")
     app.mainloop()
 
 
